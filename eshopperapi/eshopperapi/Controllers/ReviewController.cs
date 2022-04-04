9c3216245a903c2d06b2b3c35db5a5398a4008c7
Nithin Reddy Bogadi <98127677+Nithinreddy90@users.noreply.github.com>
2022-04-04 08:33:38 -0400

    Add files via upload

---
+++ eshopperapi/eshopperapi/Controllers/ReviewController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using eshopperapi.Data;
+using eshopperapi.Models;
+
+namespace eshopperapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly eshopperapiContext _context;
+
+        public ReviewController(eshopperapiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Review
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReviewModel>>> GetReviewModel()
+        {
+            return await _context.ReviewModel.ToListAsync();
+        }
+
+        // GET: api/Review/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<ReviewModel>>> GetReviewModel(int id)
+        {
+            var reviewModel = await _context.ReviewModel.Where(x => x.Product_id == id).ToListAsync();
+
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+
+            return reviewModel;
+        }
+
+        // PUT: api/Review/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReviewModel(int id, ReviewModel reviewModel)
+        {
+            if (id != reviewModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reviewModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReviewModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Review
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ReviewModel>> PostReviewModel(ReviewModel reviewModel)
+        {
+            _context.ReviewModel.Add(reviewModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReviewModel", new { id = reviewModel.Id }, reviewModel);
+        }
+
+        // DELETE: api/Review/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReviewModel(int id)
+        {
+            var reviewModel = await _context.ReviewModel.FindAsync(id);
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.ReviewModel.Remove(reviewModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReviewModelExists(int id)
+        {
+            return _context.ReviewModel.Any(e => e.Id == id);
+        }
+    }
+}
